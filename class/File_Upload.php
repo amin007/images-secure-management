5fc7cbe6eb9a641d55d197255702806285ee4cf0
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-12-01 12:06:42 +0800

    make new function

--- class/File_Upload.php
+++ class/File_Upload.php
@@ -4,6 +4,51 @@ class File_Upload
 {
 #==================================================================================================
 #--------------------------------------------------------------------------------------------------
+	# Checks if required PHP extensions are loaded. Tries to load them if not
+	private function check_phpExt()
+	{
+		if (!extension_loaded('fileinfo'))
+		{
+			# dl() is disabled in the PHP-FPM since php7 so we check if it's available first
+			if(function_exists('dl'))
+			{
+				if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+				{
+					return (!dl('fileinfo.dll')) ? false:true;
+					//if (!dl('fileinfo.dll')) { return false; }
+					//else { return true; }
+				}
+				else
+				{
+					return (!dl('fileinfo.so')) ? false:true;
+					//if (!dl('fileinfo.so')) { return false; }
+					//else { return true; }
+				}
+			}
+			else { return false; }
+		}
+		else { return true; }
+		#
+	}
+#--------------------------------------------------------------------------------------------------
+	# Handles the uploading of images
+	public function uploadImages($files)
+	{
+		//echo '<hr>Name class :' . __METHOD__ . '()<hr>';
+		# Checks if the required PHP extension(s) are loaded
+		if($this->check_phpExt())# check function line 7
+		{
+			//echo '<br> lepas ujian check_phpExt() . ';
+		}
+		else
+		{
+			array_push($this->error, "The PHP fileinfo extension isn't loaded and "
+			. "ImageUpload was unable to load it for you.");
+			$this->obj->error = $this->error;
+			return $this->obj;
+		}
+	}
+#--------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------
 #==================================================================================================
 }
