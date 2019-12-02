af7c060148edb16301f1a5a377de2c64aba97e4a
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-12-02 17:57:40 +0800

    make view image

--- upload.php
+++ upload.php
@@ -24,6 +24,33 @@ if(!empty($result->ids))
 	echo 'Your images can be viewed here:<br><br>';
     foreach($result->ids as $id)
 	{
-        echo '<br>image.php?'. $id;
+        //echo '<br>image.php?'. $id;
+        echo '<br>';
+		$k0 = URL . '?imageId=' . $id;
+		//pautanTD('_blank',$k0,null,$id,null);
+		picture('_blank',$k0,null,$id,null);
     }
+	echo '<br><a href="index.php">Back To Index</a>';
 }
+
+# for function only
+#--------------------------------------------------------------------------------------------------
+function pautanTD($target, $href, $class, $data, $iconFA)
+{
+	$t = ($target == null) ? '':' target="' . $target . '"';
+	$data = ($data == '0' or $data == null) ? '&nbsp;':$data;
+	$iconFA = ($iconFA == null) ? '':$iconFA;
+	?><a<?php echo $t ?> href="<?php echo $href ?>" class="<?php
+	echo $class ?>"><?php echo $data ?></a>|<?php
+}
+#--------------------------------------------------------------------------------------------------
+function picture($target, $href, $class, $data, $iconFA)
+{
+	$t = ($target == null) ? '':' target="' . $target . '"';
+	$data = ($data == '0' or $data == null) ? '&nbsp;':$data;
+	$iconFA = ($iconFA == null) ? '':$iconFA;
+	?><a<?php echo $t ?> href="<?php echo $href ?>" class="<?php
+	echo $class ?>"><img height="50%" width="50%" src="<?php echo $href ?>"><?php
+	echo 'data = ' . $data ?></a>|<?php
+}
+#--------------------------------------------------------------------------------------------------
